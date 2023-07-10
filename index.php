50d22702c917fc7b84a816451c74366b5c850435
Lohvet <idokolohvet@gmail.com>
2023-07-10 10:42:53 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Header</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/3734bae25c.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+            <label for="check" class="checkbtn ">
+                <i class="fa-solid fa-bars"></i>
+            </label>
+        <label for="logo" class="third">GLITO <img src="assets/logo.jpg" alt="logo"90></label>
+            <ul>
+                <li><i class="fa-solid fa-house"></i><a href="">Home</a></li>
+                <li><i class="fa-solid fa-square"></i><a href="">Categories</a></li>
+                <li><i class="fa-solid fa-cart-shopping"></i><a href="">Cart</a></li>
+                <li><i class="fa-solid fa-user"></i><a href="">Account</a></li>
+                <li><i class="fa-solid fa-phone"></i><a href="">Contact</a></li>
+            </ul>
+            <div class="other">
+                <input type="text" placeholder="Anything on GLITO">
+                <i id="last"class="fa-solid fa-magnifying-glass"></i>
+            </div>
+    </nav>
+</body>
+</html>
